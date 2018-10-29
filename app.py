5ef127582dee039783f642626a0438c94faf0592
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-10-29 17:54:09 +0900

    ADD : screenshot

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import kivy
+kivy.require('1.10.1')
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.config import Config
+from kivy.core.window import Window
+
+Window.size = (400,130)
+
+from kivy.uix.boxlayout import BoxLayout
+
 import requests, random
 
 s = ['강', '경', '고', '김', '남', '노', '도', '문', '민', '박', '방', '변', '서', '석', '설', '성', '송', '신', '여', '오', '왕', '유', '이', '장', '전', '정']
@@ -19,10 +31,34 @@ def randomNames(req_num, mine):
                 break
     return names
 
-url = 'https://kr.lovemeter.me/save-quiz-response'
-question_id = input('user to attack: ')
+def sendThis(question_id, user, crush):
+    url = 'https://kr.lovemeter.me/save-quiz-response'
+    r = requests.post(url, data={'userFullName': user, 'crushFullName': crush, 'encUserQuizId': question_id})
+    print('[+]', user, '->', crush)
+    return True
 
-mine = input('your name: ')
-for name in randomNames(int(input('number of requests: '))):
-    r = requests.post(url, data={'userFullName': mine, 'crushFullName': name, 'encUserQuizId': question_id})
-    print('[+]', mine, '->', name)
+def send(question_id, user, crush, req_num):
+    for i in range(req_num):
+        sendThis(question_id, user, crush)
+
+def sendRandom(question_id, user, req_num):
+    names = randomNames(req_num, user)
+    for crush in names:
+        sendThis(question_id, user, crush)
+
+class LblTxt(BoxLayout):
+    from kivy.properties import ObjectProperty
+    theTxt = ObjectProperty(None)
+
+class LblNum(BoxLayout):
+    from kivy.properties import ObjectProperty
+    theTxt = ObjectProperty(None)
+
+class MyApp(App):
+
+    def build(self):
+        self.root = Builder.load_file('form.kv')
+        return self.root
+
+if __name__ == '__main__':
+    MyApp().run()
